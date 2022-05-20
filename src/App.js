2e11240c947fd6fa5f103d3b376ea9de18e44cdd
Joanne <jl7105@nyu.edu>
2022-05-20 13:28:57 -0400

    installing React Router v6

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import './App.css';
 import HomeScreen from './HomeScreen';
+import {
+  BrowserRouter as Router,
+  // In react-router-dom v6, "Switch" is replaced by routes "Routes"
+  // Switch,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <HomeScreen />
+
+      <Router>
+        {/* A <Switch>/<Routes> looks through its children <Route>s and renders the first one that matches the current URL. */}
+        <Routes>
+          {/* You also need to update the Route declaration: */}
+          {/* <Route path="/" component={HomeScreen} /> */}
+          <Route path="/" exact element={<HomeScreen/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
