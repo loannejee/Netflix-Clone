c549e8bd5af7cb5bc0b4d179f3db739933ae09fa
Joanne <jl7105@nyu.edu>
2022-05-20 16:23:40 -0400

    Frontend for Login Screen Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import HomeScreen from './screens/HomeScreen';
+import LoginScreen from './screens/LoginScreen';
 import {
   BrowserRouter as Router,
   // In react-router-dom v6, "Switch" is replaced by routes "Routes"
@@ -12,13 +13,13 @@ import {
 function App() {
   return (
     <div className="app">
-
       <Router>
         {/* A <Switch>/<Routes> looks through its children <Route>s and renders the first one that matches the current URL. */}
         <Routes>
           {/* You also need to update the Route declaration: */}
           {/* <Route path="/" component={HomeScreen} /> */}
           <Route path="/" exact element={<HomeScreen/>} />
+          <Route path="/login" element={<LoginScreen/>} />
         </Routes>
       </Router>
     </div>
