544a5bbcc64a06ec50439635811b42d80a6caa6c
Joanne <jl7105@nyu.edu>
2022-05-29 01:51:16 -0400

    plansScreen frontend

--- src/screens/PlansScreen.js
+++ src/screens/PlansScreen.js
@@ -2,24 +2,28 @@ import React, { useEffect, useState } from 'react';
 import "./PlansScreen.css";
 import { collection, query, where, getDocs } from "firebase/firestore";
 import db from '../firebase';
+import { useSelector } from 'react-redux';
+// This refers to the user logged in in the current state:
+import { selectUser } from '../features/userSlice';
 
 function PlansScreen() {
   const [products, setProducts] = useState([]);
+  const user = useSelector(selectUser);
 
   useEffect(() => {
     (async () => {
       // Create a reference to the products collection:
       const productsRef = collection(db, "products");
-     
+
       // Create a query against the collection. We want active to be true:
       const q = query(productsRef, where("active", "==", true));
-     
+
       const querySnapshot = await getDocs(q);
-     
+
       // We want to create our own object literal template for products:
       const products = {};
 
-      querySnapshot.forEach( async (productDoc) => {
+      querySnapshot.forEach(async (productDoc) => {
         products[productDoc.id] = productDoc.data()
 
         // Get the prices subcollection of each product (firebase v9):
@@ -35,18 +39,39 @@ function PlansScreen() {
         });
 
         setProducts(products);
-        
+
       });
     })
-
-    // for invoking?
-    ();
+      // for invoking?
+      ();
 
   }, [])
 
+  const loadCheckout = async (priceId) => {
+    const docRef = await collection(db, "customers");
+  }
+
   return (
     <div className='plansScreen'>
-      PlansScreen
+
+      { // Convert products from object to array. Then map each key/value pair in the array to create divs and buttons for each plan:
+        Object.entries(products).map(([productId, productData]) => {
+          // TODO: add some logic to check if the user's subscription is active...
+          return (
+            <div className='plansScreen_plan'>
+
+              <div className='plansScreen_info'>
+                <h5>{productData.name}</h5>
+                <h6>{productData.description}</h6>
+              </div>
+
+              <button onClick={() => loadCheckout(productData.prices.priceId)}>Subscribe</button>
+
+            </div>
+          )
+
+        })
+      }
     </div>
   )
 }
