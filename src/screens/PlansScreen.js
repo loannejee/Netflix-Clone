23c4a5ba51f8d44d9b1dd835438cee07bde0c788
Joanne <jl7105@nyu.edu>
2022-05-31 01:02:11 -0400

    loadCheckout function

--- src/screens/PlansScreen.js
+++ src/screens/PlansScreen.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import "./PlansScreen.css";
-import { collection, query, where, getDocs } from "firebase/firestore";
+import { collection, query, where, getDocs, doc, onSnapshot, addDoc } from "firebase/firestore";
 import db from '../firebase';
 import { useSelector } from 'react-redux';
 // This refers to the user logged in in the current state:
 import { selectUser } from '../features/userSlice';
 import { loadStripe } from '@stripe/stripe-js';
+import env from "react-dotenv";
+
 
 function PlansScreen() {
   const [products, setProducts] = useState([]);
@@ -47,11 +49,114 @@ function PlansScreen() {
       // for invoking?
       ();
 
-  }, [])
+  }, []);
 
   const loadCheckout = async (priceId) => {
-    const docRef = await collection(db, "customers");
-  }
+    // To create or overwrite a single document, use the set() method:
+    const customerRef = collection(db, `customers/${user.uid}/checkout_sessions`);
+
+    const docRef = await addDoc(customerRef, {
+      price: priceId,
+      // This url is for redirecting to after successful purchase:
+      success_url: window.location.origin,
+      cancel_url: window.location.origin,
+    });
+
+    console.log(">>>", docRef)
+
+    const unsub = onSnapshot(doc(db, `customers/${user.uid}/checkout_sessions`, `${docRef.id}`), async (snap) => {
+      console.log("Current data: ", snap.data());
+
+      const { error, sessionId } = snap.data();
+      if (error) {
+        // Show an error to your customer and 
+        // inspect your Cloud Function logs in the firebase console.
+        alert(`An error occurred: ${error.message}`);
+      }
+
+      if (sessionId) {
+        // We have a session, let's redirect to Checkout
+        // Init Stripe:
+        const stripe = await loadStripe(`${env.STRIPE_PUBLISHER_KEY}`);
+        console.log("Check out stripe built-in functions!", stripe)
+        stripe.redirectToCheckout({ sessionId });
+      }
+    });
+
+
+
+
+
+    // const checkOutCollection = collection(db, "customers");
+    // const checkOutSnapshot = checkOutCollection.getDocs();
+
+    // const querySnapshot = await getDocs(customersRef);
+    // querySnapshot.forEach((doc) => {
+    //   // doc.data() is never undefined for query doc snapshots
+    //   console.log(doc.id, " => ", doc.data());
+    // });
+
+    // await setDoc(doc(db, "customers", user.uid, "checkout_sessions", "123"), {
+    //   test: doc(db, "customers", "123"),
+    // });
+
+    // setDoc(customerRef, {
+    //   price: priceId,
+    //   // This url is for redirecting to after successful purchase:
+    //   success_url: window.location.origin,
+    //   cancel_url: window.location.origin,
+    // });
+
+    // await setDoc(doc(db, "customers", user.uid, "checkout_sessions", customerRef.id), {
+    //   price: priceId,
+    //   // This url is for redirecting to after successful purchase:
+    //   // success_url: window.location.origin,
+    //   // cancel_url: window.location.origin,
+    // });
+
+    // customerRef.onSnapshot(async (snap) => {
+    //   const { error, sessionId } = snap.data();
+
+    //   if (error) {
+    //     // Show an error to your customer and 
+    //     // inspect your Cloud Function logs in the firebase console.
+    //     alert(`An error occurred: ${error.message}`);
+    //   }
+
+    //   if (sessionId) {
+    //     // We have a session, let's redirect to Checkout
+    //     // Init Stripe
+    //     const stripe = await loadStripe(env.STRIPE_PUBLISHER_KEY);
+    //     console.log("Check out stripe built-in functions!", stripe)
+    //     stripe.redirectToCheckout({ sessionId });
+    //   }
+    // })
+
+
+    // const customerSnapshot = onSnapshot(customerRef, async (snap) => {
+    //   console.log("Current data: ", snap.data());
+    //   const { error, sessionId } = snap.data();
+
+    //   if (error) {
+    //     // Show an error to your customer and 
+    //     // inspect your Cloud Function logs in the firebase console.
+    //     alert(`An error occurred: ${error.message}`);
+    //   }
+
+    //   if (sessionId) {
+    //     // We have a session, let's redirect to Checkout
+    //     // Init Stripe
+    //     const stripe = await loadStripe(env.STRIPE_PUBLISHER_KEY);
+    //     console.log("Check out stripe built-in functions!", stripe)
+    //     stripe.redirectToCheckout({ sessionId });
+    //   }
+    // });
+
+
+
+
+
+  };
 
   return (
     <div className='plansScreen'>
@@ -60,7 +165,7 @@ function PlansScreen() {
         Object.entries(products).map(([productId, productData]) => {
           // TODO: add some logic to check if the user's subscription is active...
           return (
-            <div className='plansScreen_plan'>
+            <div className='plansScreen_plan' key={`keyid_${productId}`}>
 
               <div className='plansScreen_info'>
                 <h5>{productData.name}</h5>
